43bc246a6864381bb3ab2e739e566bbf0f82bdab
Patrick-Apilukpuvadol <chimei983@hotmail.com>
2023-12-05 02:54:04 +1100

    feat: installed express and set up folders for models and controllers

---
+++ src/server.js
@@ -0,0 +1,19 @@
+// Server configuration happens in server.js
+
+
+const express = require('express');
+
+// make a server instance 
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+	response.json({
+		message:"Hello world"
+	});
+});
+
+module.exports = {
+	app
+}
